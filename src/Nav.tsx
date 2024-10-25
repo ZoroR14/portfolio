f80dcb779f1a710353e01e0624bf69ac73b2557a
Orlando Jarquin <orlandojarkiin@gmail.com>
2024-10-24 20:13:41 -0600

    first commit

---
+++ src/Nav.tsx
@@ -0,0 +1,11 @@
+export default function Nav(){
+    return (
+        <section>
+            <ul>
+                <li>Sobre mi</li>
+                <li>Experiencia</li>
+                <li>Contacto</li>
+            </ul>
+        </section>
+    )
+}
